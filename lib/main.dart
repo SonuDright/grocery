7cee584c131a8fef62c1e856c30b0a2bf92d15b6
SonuDright <144533703+SonuDright@users.noreply.github.com>
2024-10-21 15:22:49 +0530

    add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery/signup_page.dart';
 
 import 'home_page.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  HomePage(),
+      home:  SignUpScreens2(),
     );
   }
 }
