44b06b11082ac294d89139818602cf33507f171d
SonuDright <144533703+SonuDright@users.noreply.github.com>
2024-10-21 06:41:52 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        Center(child: Text("Home Page " ,style: TextStyle(color: Colors.orange,fontSize: 30),)),
+      ],),)
+    );
+  }
+}
