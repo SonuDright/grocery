9ad2a62eaeca961a7bb378386c81a74b9a549f32
SonuDright <144533703+SonuDright@users.noreply.github.com>
2024-10-21 14:03:36 +0530

    add container and text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,11 +12,20 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+
         children: [
         Center(child: Text("Home Page " ,style: TextStyle(color: Colors.red,fontSize: 30,fontWeight: FontWeight.bold ),)),
-      ],),)
+
+          Center(
+            child: Container(height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(width: 2, color: Colors.red),
+                color: Colors.orange,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            width: 200,
+            child: Center(child: Text("Sonu Dright",style: TextStyle(color: Colors.black,fontSize: 40,fontWeight: FontWeight.bold )),),
+            ),
+          )],),)
     );
   }
 }
